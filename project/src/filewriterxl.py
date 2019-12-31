2a8415cd21d73659a3dc395521c286531bf6800b
Clayton <herbsca1@gmail.com>
2019-12-31 10:41:53 +0800

    output folder path existence dependency removed

--- project/src/filewriterxl.py
+++ project/src/filewriterxl.py
@@ -1,9 +1,11 @@
 from openpyxl import Workbook
+import os
 
 class FileWriterXL:
     def __init__(self, name, fieldnames):
         dirname = os.path.dirname(name)
         if not os.path.exists(dirname):
+            print(f'creating directory: {dirname}')
             os.mkdir(dirname)
         self.filename = f'{name}.xlsx'
         self.headers = FileWriterXL.setHeaders(fieldnames)
