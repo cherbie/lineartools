deaff93d8bfa43f8353c46fe66b1332d30e2ec19
Clayton <herbsca1@gmail.com>
2020-01-05 13:31:40 +0800

    added pyinstaller bundler

--- project/app.py
+++ project/app.py
@@ -1,14 +1,21 @@
-import sys, os
+import sys, os, time
 from src import FileReader, workbookParser, ConfigParser
 
+errstr = 'AN ERROR HAS OCCURED!\n\nError reporting may have been provided above. Please correct these errors.\n\nWould you like to try again: (y/N)\n'
+
 def main():
-    if len(sys.argv) < 2:
+    if len(sys.argv) == 1:
+        configFile = input('Location of CONFIG file (.json): ')
+    elif len(sys.argv) > 2:
         print('Usage: python3 app.py configfilelocation')
-        sys.exit()
+        raise Exception('INCORRECT NUMBER OF ARGUMENTS SPECIFIED')
+    else:
+        configFile = sys.argv[1]
     
-    configFile = sys.argv[1]
     if not os.path.exists(configFile):
-        raise IOError
+        print('Configuration file could not be found. Please review the file path.')
+        raise Exception('CONFIG FILE PATH ERROR. THE SPECIFIED PATH DOES NOT EXIST.\n')
+    
     print(' ... reading config file')
     config = ConfigParser(configFile)
     print(' ... reading excel file')
@@ -16,5 +23,14 @@ def main():
     print(' ... parsing file data')
     workbookParser(wb_reader, config) # business logic
     print(' ... process complete')
+    sleep(10)
 
-main()
+value = 'y'
+while value == 'y':
+    try:
+        main()
+    except Exception as err:
+        print('--------------------')
+        print(err)
+        print('--------------------')
+        value = input(errstr)
